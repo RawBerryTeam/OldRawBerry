2663417c0f1449e074d0caf719c4516ad0f7d1ee
Gorciu <gorciuyt@gmail.com>
2024-10-05 05:26:29 +0200

    declarations and header files

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,21 +1,9 @@
 #include <stdint.h>
-#define MULTIBOOT_HEADER_MAGIC 0x1BADB002
-#define MULTIBOOT_HEADER_FLAGS 0x00000003
-#define MULTIBOOT_CHECKSUM -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+#include "main.h"
 
-typedef struct {
-    uint32_t magic;
-    uint32_t flags;
-    uint32_t checksum;
-} multiboot_header_t __attribute__((packed));
-
-multiboot_header_t multiboot_header __attribute__((section(".multiboot"))) = {
-    .magic = MULTIBOOT_HEADER_MAGIC,
-    .flags = MULTIBOOT_HEADER_FLAGS,
-    .checksum = MULTIBOOT_CHECKSUM,
-};
-
-#define VGA_BUFFER 0xb8000
+void print_string(const char* str);
+void kernel_main(void);
+void panic(const char* message)
 
 void print_string(const char* str) {
     uint16_t* vga_buffer = (uint16_t*)VGA_BUFFER;
