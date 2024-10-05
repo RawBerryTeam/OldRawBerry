05b18ebb1fdc7f17cb566fde02a76822f01379a5
Goldjakyt <81904199+Goldjakyt@users.noreply.github.com>
2024-10-05 16:53:20 +0200

    WHY THIS DON'T WORK

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -1,42 +1,21 @@
 #include <stdint.h>
 #include "main.h"
+#include "drivers/display.h"
+#include "util.h"
 
-void print_string(const char* str);
 void kernel_main(void);
 void panic(const char* message);
 
-void init_vga_buffer() {
-    uint16_t* vga_control = (uint16_t*)0x3D4;
-    *vga_control = 0x03;
-    uint16_t* vga_buffer = (uint16_t*)0xB8000;
-    for (int i = 0; i < 80 * 25; i++) {
-        vga_buffer[i] = 0x0200;
-    }
-}
-
-void print_string(const char* str) {
-    uint16_t* vga_buffer = (uint16_t*)0xB8000;
-    int offset = 0;
-    while (*str) {
-        if (offset >= 80 * 25) {
-            break;
-        }
-        vga_buffer[offset] = (*str & 0xFF) | (0x0F << 8);
-        offset++;
-        str++;
-    }
-    if (offset < 80 * 25) {
-        vga_buffer[offset] = ('\n' & 0xFF) | (0x0F << 8);
-    }
-}
-
 void kernel_main() {
-    init_vga_buffer();
-    print_string("Hello RawBerry");
-    while (1) {}
+    clear_screen();
+    char* line;
+    for (int i = 1; i <= 35; ++i) {
+        int_to_string(i, line, 50);
+        print_string(line);
+        print_nl();
+    }
 }
 
 void panic(const char* message) {
-    print_string("Panic");
     while (1) {}
 }
