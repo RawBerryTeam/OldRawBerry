2f2e2d83e1c77be667997b336aade5ed56a8a0ab
Fivvee0 <mewaqin98@gmail.com>
2024-10-05 15:22:42 +0200

    IDK WHY THIS WORK

--- src/kernel/drivers/ports.h
+++
@@ -1,4 +0,0 @@
-extern unsigned char port_byte_in (unsigned short port);
-extern void port_byte_out (unsigned short port, unsigned char data);
-extern unsigned short port_word_in (unsigned short port);
-extern void port_word_out (unsigned short port, unsigned short data);
